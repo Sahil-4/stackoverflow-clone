c2ad699368ca02ebb8eb63e3a6f0d0ea0ba5a769
Sahil-4 <www.sahil.875020@gmail.com>
2022-09-25 16:50:29 +0530

    created users page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import Tags from "./Pages/Tags";
+import Users from "./Pages/Users";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/questions" element={<Home />}></Route>
         <Route path="/tags" element={<Tags />}></Route>
-        <Route path="/users" element={<div />}></Route>
+        <Route path="/users" element={<Users />}></Route>
         <Route path="/login" element={<div />}></Route>
       </Routes>
     </Router>
---
+++ frontend/src/Pages/Users.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Sidebar from "../Components/Sidebar";
+import UserImage from "../assets/user.png";
+import { Link } from "react-router-dom";
+
+const Users = () => {
+  const Profile = () => {
+    return (
+      <div className="profile-card">
+        <div className="profile-image-container">
+          <img src={UserImage} alt="user" />
+        </div>
+        <div className="info">
+          <Link to="users/Alpha">
+            <h4>Alpha</h4>
+          </Link>
+          <p>Python Lover</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="main">
+      <Sidebar />
+      <div className="users-profile-container">
+        <h1>All Users</h1>
+        <div className="all-profiles">
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
